e4f2d9698f5e9b8a920cf9f7d2b3fee67355da0c
KinCod <100110821+KinCod@users.noreply.github.com>
2024-07-05 12:24:56 +0530

    added the cookie Parser middleWare

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotEnv from "dotenv";
+import cookieParser from "cookie-parser";   //middle ware to access the cookies ()
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+
 
 const app = express();          //app is an express function
 const PORT = process.env.PORT || 5000; 
@@ -13,6 +15,7 @@ dotEnv.config();
 //always keep this above the routes vrna req.body nhi kar paoge
 app.use(express.json()); //to parse incoming requests from JSON payloads (from request.body)
                             //Mtlb agar kisi route se data request krra hu tab vo directly nhi le skta mai but iss middleware ko use karna hoga (Noice)  
+app.use(cookieParser());
 
 //making routes for auth and that will be done using a middleware
 app.use("/api/auth", authRoutes);       //as soon as api/auth is seen we go to the authRoutes middleware
